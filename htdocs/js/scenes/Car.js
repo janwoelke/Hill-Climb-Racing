85de3f1fcfcda8e0561947bb0c3408ca059de727
Jan <94203296+janwoelke@users.noreply.github.com>
2022-09-07 17:21:53 +0200

    Add files via upload

---
+++ htdocs/js/scenes/Car.js
@@ -0,0 +1,115 @@
+export class Car {
+    constructor(scene, engine, world) {
+        this.scene = scene;
+        this.engine = engine;
+        this.world = world;
+        this.wheels = [];
+        this.matterWheels = [];
+        this.matterSprings = [];
+        let factor = 0.2;
+        this.wheels.push(scene.add.sprite(290, 310, "wheel"));
+        this.wheels.push(scene.add.sprite(310, 310, "wheel"));
+        this.chassis = scene.add.sprite(300, 300, "chassis");
+        this.chassis.setScale(factor);
+        this.wheels.forEach(wheel => wheel.setScale(factor));
+        let group = Matter.Body.nextGroup(true);
+        let xx = 300;
+        let yy = 300;
+        let width = 1200 * factor;
+        let height = 425 * factor;
+        let wheelAOffset = -360 * factor;
+        let wheelBOffset = 317 * factor;
+        let wheelYOffset = 135 * factor;
+        let wheelSize = 280 * 0.5 * factor; // 28.07.2022: Faktor 0.5 eingefügt, da 280px der Durchmesser der Graphik ist, wheelSize aber der Radius des Matter-Kreises
+        let body;
+        this.matterCar = Matter.Composite.create({ label: 'Car' }),
+            // x, y: center of rectangle
+            body = Matter.Bodies.rectangle(xx, yy, width, height, {
+                collisionFilter: {
+                    group: group
+                },
+                chamfer: {
+                    radius: height * 0.5
+                },
+                density: 0.0002
+            });
+        let wheelA = Matter.Bodies.circle(xx + wheelAOffset, yy + wheelYOffset, wheelSize, {
+            collisionFilter: {
+                group: group
+            },
+            friction: 0.8,
+        });
+        let wheelB = Matter.Bodies.circle(xx + wheelBOffset, yy + wheelYOffset, wheelSize, {
+            collisionFilter: {
+                group: group
+            },
+            friction: 0.8,
+            restitution: 0.2
+        });
+        //Masse des Autos normal: 1
+        this.matterWheels.push(wheelA, wheelB);
+        this.matterChassis = body;
+        //Matter.Body.setMass(this.matterChassis,1);
+        let axelOffset = 20;
+        let constraint_legth = 20;
+        let constraint_stiffness = 0.6;
+        let axelA1 = Matter.Constraint.create({
+            bodyB: body,
+            pointB: { x: wheelAOffset - axelOffset, y: wheelYOffset },
+            bodyA: wheelA,
+            stiffness: constraint_stiffness,
+            length: constraint_legth
+        });
+        let axelA2 = Matter.Constraint.create({
+            bodyB: body,
+            pointB: { x: wheelAOffset + axelOffset, y: wheelYOffset },
+            bodyA: wheelA,
+            stiffness: constraint_stiffness,
+            length: constraint_legth
+        });
+        let axelB1 = Matter.Constraint.create({
+            bodyB: body,
+            pointB: { x: wheelBOffset - axelOffset, y: wheelYOffset },
+            bodyA: wheelB,
+            stiffness: constraint_stiffness,
+            length: constraint_legth
+        });
+        let axelB2 = Matter.Constraint.create({
+            bodyB: body,
+            pointB: { x: wheelBOffset + axelOffset, y: wheelYOffset },
+            bodyA: wheelB,
+            stiffness: constraint_stiffness,
+            length: constraint_legth
+        });
+        //@ts-ignore
+        Matter.Composite.addBody(this.matterCar, body);
+        //@ts-ignore
+        Matter.Composite.addBody(this.matterCar, wheelA);
+        //@ts-ignore
+        Matter.Composite.addBody(this.matterCar, wheelB);
+        //@ts-ignore
+        Matter.Composite.addConstraint(this.matterCar, axelA1);
+        //@ts-ignore
+        Matter.Composite.addConstraint(this.matterCar, axelA2);
+        //@ts-ignore
+        Matter.Composite.addConstraint(this.matterCar, axelB1);
+        //@ts-ignore
+        Matter.Composite.addConstraint(this.matterCar, axelB2);
+    }
+    adjustPhaserObjectsToMatter() {
+        let chassisPosition = this.matterChassis.position;
+        this.chassis.setPosition(chassisPosition.x, chassisPosition.y);
+        let chassisAngle = this.matterChassis.angle;
+        this.chassis.setAngle(chassisAngle / Math.PI * 180);
+        for (let i = 0; i < this.wheels.length; i++) {
+            let phaserWheel = this.wheels[i];
+            let matterWheel = this.matterWheels[i];
+            let pos = matterWheel.position;
+            phaserWheel.setPosition(pos.x, pos.y);
+            phaserWheel.setAngle(matterWheel.angle / Math.PI * 180);
+        }
+    }
+    update() {
+    }
+}
+//# sourceMappingURL=Car.js.map
---
+++ src/scenes/Runninggame.ts
@@ -0,0 +1,304 @@
+import { Car } from "./Car.js";
+
+export default class Runninggame extends Phaser.Scene {
+
+
+    
+    engine: Matter.Engine;
+    world: Matter.World;
+    runner: Matter.Runner;
+
+    Fullscreenevent: Phaser.Input.Keyboard.Key;
+    map: Phaser.Tilemaps.Tilemap;
+
+    bodies: { matterBody: Matter.Body, phaserShape: Phaser.GameObjects.Shape }[] = [];
+    matterBodies:Matter.Body[] = []
+
+    car: Car;
+    spawnpoint;
+    
+    //Steuerung
+    cursors: Phaser.Types.Input.Keyboard.CursorKeys;
+    keyA;
+    keyD;
+
+    //Coins
+    coinsnumber: Phaser.GameObjects.Text;
+    coinscounter = 0;
+
+    //Distance
+    distance: Phaser.GameObjects.Text;
+    distancenumber: Phaser.GameObjects.Text;
+    distancecounter = 0;
+    
+    //Background
+    private paralaxbackgrounds: {ratioX: number, sprite: Phaser.GameObjects.TileSprite} [] = []
+
+    //Fahren des Autos MAX_SPEED normal: 0.75
+    readonly MAX_SPEED = 0.3
+    readonly MAX_SPEED_BACKWARDS = this.MAX_SPEED * 0.3
+    readonly ACCELERATION = this.MAX_SPEED / 130
+    readonly ACCELERATION_BACKWARDS = this.ACCELERATION * 0.3
+   
+    gas = {
+        right: false,
+        left: false
+      }
+      wheelsDown = {
+        rear: false,
+        front: false
+      }
+
+
+    constructor() {
+
+    super("Runninggame") 
+
+
+    }
+
+
+
+    preload() {
+
+        this.load.image("spritesheet", "/htdocs/assets/images/spritesheet.png")
+        this.load.tilemapTiledJSON("map", "/htdocs/assets/map/map.json")
+        this.load.image("chassis", "/htdocs/assets/images/Car.png")
+        this.load.image("wheel", "/htdocs/assets/images/Wheel.png")
+        this.load.image("background_sky", "/htdocs/assets/images/background_sky.png")
+        this.load.image("background_mountains", "/htdocs/assets/images/background_mountains.png")
+    }
+
+
+
+    create() {
+        
+        const screenCenterX = this.cameras.main.worldView.x + this.cameras.main.width / 2;
+        const screenCenterY = this.cameras.main.worldView.y + this.cameras.main.height / 2;
+        
+        // this.cameras.main.startFollow(this.car,true, 0.5, 0.5, 0.5, 0.5)
+        // this.cameras.main.setRoundPixels(true);
+        
+        this.add.image(0, 0, "background_sky").setOrigin(0,0).setScale(2.5).setDepth(-3).setScrollFactor(0)
+
+        this.paralaxbackgrounds.push( {
+            ratioX: 0.1,
+            sprite: this.add.tileSprite(0, 690, innerWidth, 400, "background_mountains").setOrigin(0,0).setScrollFactor(0).setDepth(-3).setAlpha(0.7)
+
+        })
+       
+        this.distance = this.add.text(screenCenterX, 50, "DISTANCE", {
+            fontFamily: "hillclimbracing",
+            fontSize: "80px",
+            color: "#FFFFFF",
+            align: "center",
+            stroke: "#000000",
+            strokeThickness: 10,
+
+        }).setScrollFactor(0).setOrigin(0.5)
+        
+        this.distancenumber = this.add.text(screenCenterX, 120, "" + this.distancecounter, {
+
+
+            fontFamily: "hillclimbracing",
+            fontSize: "80px",
+            color: "#FFFFFF",
+            align: "center",
+            stroke: "#000000",
+            strokeThickness: 10,
+
+        }).setScrollFactor(0).setOrigin(0.5)
+
+        this.engine = Matter.Engine.create({
+            gravity: {y: 0.2}
+        })
+
+        this.world = this.engine.world;
+
+        this.map = this.make.tilemap({
+            key: "map"
+        })
+
+        const spritesheet = this.map.addTilesetImage("HillClimbRacing_testmap", "spritesheet");
+
+        const base = this.map.createLayer("Map", spritesheet)
+
+        
+        let objectLayer = this.map.getObjectLayer("Collisions")
+
+
+        let polygon = objectLayer.objects.find(obj => obj.name == "Polygon1")
+        let polygonVectors:Phaser.Types.Math.Vector2Like[] = polygon.polygon;
+
+        /**
+         * Tiled speichert ein Polygon als Liste von Punkten mit Koordinaten relativ zu einem "Ankerpunkt" mit den Koordinaten (polygon.x, polygon.y).
+         * Da wir die Polygonpunkte in absoluten Koordinaten brauchen, addieren wir zunächst die Ankerpunkte:
+         */
+        for(let p of polygonVectors){
+            p.x += polygon.x;
+            p.y += polygon.y;
+        }
+
+//        let polygonVectors:Phaser.Types.Math.Vector2Like[] = [{x: 0, y: 500}, {x: 800, y: 500}, {x: 800, y: 1000}, {x: 0, y: 1000} ];
+
+        // let poly = new Phaser.Geom.Polygon(polygonVectors);
+        // let graphics = this.add.graphics();
+        // graphics.fillStyle(0xff0000);
+        // graphics.fillPoints(poly.points, true);
+    
+        let polygonString = polygonVectors.map(p => " " + p.x + " " + p.y ).join().substring(1);
+        
+        //@ts-ignore
+        let vertices = Matter.Vertices.fromPath(polygonString);
+        
+        /**
+         * Die Methode Matter.Bodies.fromVertices verschiebt den Körper so, dass sein Schwerpunkt an den Koordinaten (0,0) zu liegen kommt.
+         * Leider speichert es nirgends ab, um wie viel verschoben wurde. Um dies zu ermitteln, berechnen wir die BoundingBox des Polygons
+         * (boundsBefore) und subtrahieren deren linke obere Ecke von der BoundinBox des Körpers. Diese Verschiebung machen wir dann rückgängig.
+         */
+        let boundsBefore = Matter.Bounds.create(vertices);
+        
+        let polygonBody = Matter.Bodies.fromVertices(0, 0, [vertices])
+        let dx = polygonBody.bounds.min.x - boundsBefore.min.x;
+        let dy = polygonBody.bounds.min.y - boundsBefore.min.y;
+
+        Matter.Body.setPosition(polygonBody, {x: -dx, y: -dy});
+        polygonBody.friction = 0.5;
+
+
+        polygonBody.isStatic = true;
+        this.matterBodies.push(polygonBody);
+
+        Matter.Composite.add(this.world, polygonBody);
+        
+        this.car = new Car(this, this.engine, this.world);
+        Matter.Composite.add(this.world, this.car.matterCar);
+        
+        // this.coinsnumber = this.add.text(150, 50, "" + this.coinscounter, {
+
+
+        //     stroke: "#ffffff",
+        //     strokeThickness: 5,
+        //     fontFamily: "hillclimbracing",
+        //     color: "#ffffff",
+        //     align: "center",
+        //     fontSize: "55px"
+
+
+        // }).setScrollFactor(0).setOrigin(0.5)
+
+        // let collectables = this.map.getObjectLayer("Collectables");
+        // let coins = collectables.objects.find(o => o.type == "coins");
+        // var collectableslayer = this.map.createFromObjects("collectables", [{
+        //     gid: 2,
+        //     key: "coin"
+        // }
+        // ])
+       
+        // collectableslayer.forEach( (collectable:Phaser.Physics.Arcade.Sprite) => {
+        //     this.physics.world.enable(collectable);
+        //     //@ts-ignore
+        //     collectable.body.setAllowGravity(false);
+            
+        //     this.physics.add.overlap(car.matterChassis, collectable, this.collect, null, this)
+        // })
+        // this.physics.add.collider(base, collectableslayer, null, null, this)
+        
+        // collect(this.car.matterChassis, collectables) {
+
+        
+        //     if(collectables.texture.key == "coin") {
+                
+        //         collectables.destroy(true)
+        //         this.coinscounter++;
+        //         this.coinsnumber.setText("" + this.coinscounter)
+                
+        //     }
+        // }
+
+        this.Fullscreenevent = this.input.keyboard.addKey("F");
+        
+        this.Fullscreenevent.on("down", function() {
+            
+            
+            if(this.scale.isFullscreen) {
+                
+                this.scale.stopFullscreen();
+                
+            } else {
+                
+                this.scale.startFullscreen();
+                
+            }
+            
+        }, this);
+
+        this.keyA = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
+        this.keyD = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
+        this.cursors = this.input.keyboard.createCursorKeys();
+    }
+
+   
+
+
+    
+    
+
+    update() {
+
+        Matter.Engine.update(this.engine, 1000/60);
+        this.car.adjustPhaserObjectsToMatter();
+      
+        let wheelA = this.car.matterWheels[0];
+        let wheelB = this.car.matterWheels[1];
+        let carBody = this.car.matterChassis;
+        
+        this.cameras.main.centerOn(wheelA.position.x + 300, wheelA.position.y - 100)
+        // set the smooth zoom
+        const wheelRear = this.car.matterChassis
+        const currentZoom = this.cameras.main.zoom
+        let zoom = 1 - wheelRear.angularVelocity / 1.65
+        if (zoom > currentZoom + currentZoom * 0.0022) zoom = currentZoom + currentZoom * 0.0022
+        else if (zoom < currentZoom - currentZoom * 0.0022) zoom = currentZoom - currentZoom * 0.0022
+        if (zoom > 1) zoom = 1
+        if (zoom < 0.6) zoom = 0.6
+        this.cameras.main.setZoom(zoom)
+
+        for(let i = 0; i < this.paralaxbackgrounds.length; ++i) {
+            const pbg = this.paralaxbackgrounds[i];
+
+            pbg.sprite.tilePositionX = this.cameras.main.scrollX * pbg.ratioX
+        }
+
+        
+        //angularVelocity normal: 0.005
+        let angularVelocity = 0.005
+
+        if (this.keyD.isDown || this.cursors.right.isDown) {
+          let newSpeed = 
+            wheelB.angularSpeed <= 0 ? this.MAX_SPEED / 10 : wheelB.angularSpeed + this.ACCELERATION
+          if (newSpeed > this.MAX_SPEED) newSpeed = this.MAX_SPEED
+          Matter.Body.setAngularVelocity(wheelB, newSpeed)
+          Matter.Body.setAngularVelocity(wheelA, newSpeed)
+         
+        this.distancecounter = this.distancecounter + 0.03 
+        this.distancenumber.setText("" + Math.round(this.distancecounter))
+        
+        //   if (!this.wheelsDown.rear && !this.wheelsDown.front) Matter.Body.setAngularVelocity(carBody, -angularVelocity)
+        } else if (this.keyA.isDown || this.cursors.left.isDown) {
+          let newSpeed =
+            wheelB.angularSpeed <= 0 ? this.MAX_SPEED_BACKWARDS / 10 : wheelB.angularSpeed + this.ACCELERATION_BACKWARDS
+          if (newSpeed > this.MAX_SPEED_BACKWARDS) newSpeed = this.MAX_SPEED_BACKWARDS
+    
+          Matter.Body.setAngularVelocity(wheelB, -newSpeed)
+          Matter.Body.setAngularVelocity(wheelA, -newSpeed)
+          if(this.distancecounter > 0) {
+          this.distancecounter = this.distancecounter - 0.03
+          this.distancenumber.setText("" + Math.round(this.distancecounter))
+          }
+        //   if (!this.wheelsDown.rear && !this.wheelsDown.front) Matter.Body.setAngularVelocity(carBody, angularVelocity)
+
+    }
+
+}
+}
